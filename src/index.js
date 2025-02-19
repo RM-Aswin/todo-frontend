612502dab01d67ad22f9d2d50179c51b4c2ccf73
RM-Aswin <aswinrm.mech@gmail.com>
2025-02-19 12:37:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import ErrorBoundary from './ErrorBoundary';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';  // Make sure the path is correct
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
